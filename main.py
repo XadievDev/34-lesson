b26b3af2a825236a4f470a8c45d8a9e7151e11f8
Amirbek Xadiev <93701665+XadievDev@users.noreply.github.com>
2021-11-19 06:16:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#XadievDev
+#34-lesson.JSOn
+
+#-----------------------------------------------------#
+
+# import json
+
+# x = 10
+# x_json = json.dumps(x)
+
+# ism = "anvar"
+# ism_json = json.dumps(ism)
+
+# sonlar = [12, 45, 23, 67]
+# sonlar_json = json.dumps(sonlar)
+
+#-----------------------------------------------------#
+
+# import json
+
+# bemor = {
+#   "ism": "Alijon Valiyev",
+#   "yosh": 30,
+#   "oila": True,
+#   "farzandlar": ("Ahmad","Bonu"),
+#   "allergiya": None,
+#   "dorilar": [
+#     {"nomi": "Analgin", "miqdori": 0.5},
+#     {"nomi": "Panadol", "miqdori": 1.2}
+#   ]
+# }
+
+# with open('sonlar.json','w') as f:
+#     json.dumps(sonlar,f)
